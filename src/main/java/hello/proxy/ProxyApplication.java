98bb459952bf3fddc6640c36f09754a7b7f51022
Jrock30 <jrg8989@knou.ac.kr>
2021-11-29 00:09:27 +0900

    feat: V1 인터페이스와 구현 클래스 - 스프링 빈으로 수동등록

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,8 +1,23 @@
 package hello.proxy;
 
+import hello.proxy.config.AppV1Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+/**
+ * @Import(AppV1Config.class) : 클래스를 스프링 빈으로 등록한다. 여기서는 AppV1Config.class 를 스프링 빈으로 등록한다.
+ * 일반적으로 @Configuration 같은 설정 파일을 등록할 때 사용하지만, 스프링 빈을 등록할 때도 사용할 수 있다.
+ *
+ * @SpringBootApplication(scanBasePackages = "hello.proxy.app") : @ComponentScan 의 기능과 같다.컴포넌트 스캔을 시작할 위치를 지정한다.
+ * 이 값을 설정하면 해당 패키지와 그 하위 패키지를 컴포넌트 스캔한다. 이 값을 사용하지 않으면 ProxyApplication 이 있는 패키지와 그 하위 패키지를 스캔한다. 참고로 v3 에서 지금 설정한 컴포넌트 스캔 기능을 사용한다.
+ *
+ * @Configuration 은 내부에 @Component 애노테이션을 포함하고 있어서 컴포넌트 스캔의 대상이 된다.
+ * 따라서 컴포넌트 스캔에 의해 hello.proxy.config 위치의 설정 파일들이 스프링 빈으로 자동 등록 되지 않도록 컴포넌스 스캔의 시작 위치를
+ * !!!!!! scanBasePackages=hello.proxy.app !!!!!! 로 설정해야 한다. 그리고 필요한 Config 만 빈으로 등록하기 위해
+ * !!!!! @Import(AppV1Config.class) !!!!! 를 사용한 것이다.
+ */
+@Import(AppV1Config.class) // Config Import 스프링 빈으로 등록
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
