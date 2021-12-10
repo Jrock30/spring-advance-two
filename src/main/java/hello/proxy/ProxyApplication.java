d1d1daecdbb87ab71d3052c6fe3dcbb86bb25fca
Jrock30 <jrg8989@knou.ac.kr>
2021-12-11 01:09:40 +0900

    feat: ProxyFactory, Pointcut, Advice, Advisor

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -6,6 +6,8 @@ import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -29,7 +31,9 @@ import org.springframework.context.annotation.Import;
 //@Import(InterfaceProxyConfig.class) // proxy config 스프링 빈으로 등록
 //@Import(ConcreteProxyConfig.class) // proxy config 스프링 빈으로 등록
 //@Import(DynamicProxyBasicConfig.class) // proxy config 스프링 빈으로 등록
-@Import(DynamicProxyFilterConfig.class) // proxy config 스프링 빈으로 등록
+//@Import(DynamicProxyFilterConfig.class) // proxy config 스프링 빈으로 등록
+//@Import(ProxyFactoryConfigV1.class) // proxy config 스프링 빈으로 등록
+@Import(ProxyFactoryConfigV2.class) // proxy config 스프링 빈으로 등록
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
